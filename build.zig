56e4d60bb2fdd209bb2333ea90a79c25f3c5389d
Noxwell <beebono@yahoo.com>
2026-05-05 04:31:01 -0500

    Attempt repackage for SDL2

--- build.zig
+++ build.zig
@@ -3,14 +3,6 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const sanitize_c_type = @typeInfo(@FieldType(std.Build.Module.CreateOptions, "sanitize_c")).optional.child;
-    const sanitize_c = b.option(sanitize_c_type, "sanitize-c", "Detect undefined behavior in C");
-    const harfbuzz_enabled = b.option(bool, "enable-harfbuzz", "Use HarfBuzz to improve text shaping") orelse true;
-    const preferred_linkage = b.option(
-        std.builtin.LinkMode,
-        "preferred_linkage",
-        "Prefer building statically or dynamically linked libraries (default: static)",
-    ) orelse .static;
 
     const upstream = b.dependency("SDL_ttf", .{});
 
@@ -18,30 +10,20 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .link_libc = true,
-        .sanitize_c = sanitize_c,
+        .strip = true,
     });
 
     const lib = b.addLibrary(.{
-        .name = "SDL3_ttf",
-        .version = .{ .major = 3, .minor = 2, .patch = 2 },
-        .linkage = preferred_linkage,
+        .name = "SDL2_ttf",
+        .version = .{ .major = 2, .minor = 25, .patch = 0 },
+        .linkage = .static,
         .root_module = mod,
     });
-    mod.addIncludePath(upstream.path("include"));
-    mod.addIncludePath(upstream.path("src"));
+    mod.addIncludePath(upstream.path("."));
     mod.addCSourceFiles(.{
-        .root = upstream.path("src"),
+        .root = upstream.path("."),
         .files = srcs,
     });
-
-    if (harfbuzz_enabled) {
-        const harfbuzz_dep = b.dependency("harfbuzz", .{
-            .target = target,
-            .optimize = optimize,
-        });
-        mod.linkLibrary(harfbuzz_dep.artifact("harfbuzz"));
-        mod.addCMacro("TTF_USE_HARFBUZZ", "1");
-    }
 
     const freetype_dep = b.dependency("freetype", .{
         .target = target,
@@ -49,12 +31,11 @@ pub fn build(b: *std.Build) void {
     });
     mod.linkLibrary(freetype_dep.artifact("freetype"));
 
-    const sdl = b.dependency("SDL", .{
+    const sdl_dep = b.dependency("SDL", .{
         .target = target,
         .optimize = optimize,
-        .preferred_linkage = preferred_linkage,
-    }).artifact("SDL3");
-    mod.linkLibrary(sdl);
+    });
+    mod.linkLibrary(sdl_dep.artifact("SDL2"));
 
     lib.installHeadersDirectory(upstream.path("include"), "", .{});
 
@@ -62,10 +43,7 @@ pub fn build(b: *std.Build) void {
 }
 
 const srcs: []const []const u8 = &.{
-    "SDL_gpu_textengine.c",
-    "SDL_hashtable.c",
-    "SDL_hashtable_ttf.c",
-    "SDL_renderer_textengine.c",
-    "SDL_surface_textengine.c",
+    "glfont.c",
+    "showfont.c",
     "SDL_ttf.c",
 };
